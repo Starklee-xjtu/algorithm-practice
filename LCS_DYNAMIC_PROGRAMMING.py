f1560d08db513716f3877fc54435029b781900b7
SunnySTK <starkleeprince@foxmail.com>
2019-04-25 13:42:35 +0800

    Update LCS_DYNAMIC_PROGRAMMING.py
    finished version of LCS problem

--- LCS_DYNAMIC_PROGRAMMING.py
+++ LCS_DYNAMIC_PROGRAMMING.py
@@ -1,16 +1,22 @@
 
 import numpy as np
+import math
+
+
 def string_reverse(string):
     return string[::-1]
 
 def LCS_str(s1):
     num1=len(s1)
-    arr = [[0 for i in range(num1 + 1)] for j in range(num1 + 1)]
-    temp = [i for i in range(num1 + 1)]
-    max1 = 0
-    p = 0
+    lists = [[] for i in range(num1*2-1)]
+    arr = [[0 for i in range(num1)] for j in range(num1)]
+    arnp= np.asarray(arr)
+    temp = [i for i in range(num1)]
+    temp0 = np.asarray(temp)
     s2 = string_reverse(s1)
-    cnt =[0 for i in range(num1)];
+    cnt =[0 for i in range(num1*2-1)];
+    ntemp=num1;
+
     for i in range(len(s2)):
         for j in range(len(s1)):
             if s1[i] == s2[j] and i != num1 - j -1:
@@ -18,16 +24,40 @@ def LCS_str(s1):
             else:
                 arr[i][j] = 0
 
-    for i in range(num1+1):
-        for j in range(i):
+    for i in range(num1*2-1):
+        if i < num1 :
+            for j in range(i+1):
+                #print ([num1-1-temp0[i]+j],[j])
+                lists[i].append (arr[num1-1-temp0[i]+j][j])
+        else:
+            ntemp=ntemp-1;
+            for j in range(ntemp):
+                lists[i].append (arr[j][j+1+i-num1])
 
-            print (arr[7-temp[i]+j+1][j])
+    for i in range(num1 * 2 - 1):
+        cnt[i]=lists[i].count(1)
 
+    num4 = max (cnt)
+    i=cnt.index(num4)
+    cnt2 =[0 for i in range(num4)]; #存储重复字段
+
+    ntemp = num1;
+    num3=0;
+    if i < num1 :
+        for j in range(i+1):
+            if arr[num1-1-temp0[i]+j][j] == 1:
+                cnt2[num3] = (s1[num1-1-temp0[i]+j])
+                num3=num3+1
+    else:
+        ntemp=ntemp-i+num1-1;
+        for j in range(ntemp):
+            if arr[j][j+1+i-num1] == 1:
+                cnt2 [num3] = (s1[j])
+                num3 = num3+1
+
+    print (cnt2[0:math.floor(num4/2)])
+
+    print('  finishied')
             # if arr[i][num1-j-1] == arr[i+1][num1-j]:
             #     cnt[i] = cnt[i]+1
-
-
-
-
-
-print(LCS_str('REDIVIDE'))
+print(LCS_str('REDIVIDEAAAAAAA'))
