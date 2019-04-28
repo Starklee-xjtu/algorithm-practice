929b467398ea99691f68e3beb8cc54e4e58a41bb
SunnySTK <starkleeprince@foxmail.com>
2019-04-28 18:11:11 +0800

    convex
    complete basic structure

--- CONVEX.py
+++ CONVEX.py
@@ -4,6 +4,14 @@
 #          Lower Tangent - line joining (0,-2) and (4,0)
 import math
 
+def sign_ret(num):
+    if(num==0):
+        return 0
+    if(num > 0):
+        return 1
+    else:
+        return -1
+
 def takefirst(elem):
     return elem[0]
 
@@ -16,6 +24,25 @@ def find_lower_tangent(a,b):
 def find_convex(s0):
     return(s1)
 
+def brutehull(li):
+    hull=[]
+    pos=0
+    neg=0
+    for i in range(len(li)):
+        for j in range(i+1,len(li)):
+            p1=li[i]
+            p2=li[j]
+
+            for k in range(len(li)):
+                p3=li[k]
+                if (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1])>=0:
+                    pos = pos+1
+                if (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1])<=0:
+                    neg = neg+1
+            if pos == len(li) or neg == len(li):
+                hull.append(p1)
+
+    return hull
 
 lists = [[2, 2], [3, 3], [5, 2], [4, 0], [3, 1], [-1, 0], [0, 1], [1, 0], [0, -2]]
 lists.sort(key=takefirst)
@@ -25,7 +52,9 @@ for i in range (num0):
     lists_temp[i]=lists[i:i+5]
 
 if len(lists) - 5*num0 != 0:
-    lists_temp.append(lists[5*num0:len(lists)-1])
+    lists_temp.append(lists[5*num0:len(lists)])
+
+hull1=brutehull(lists)
 
 
 print('finished')
