aa4b3053a98d9483d9923a9cd92592c1e873bbb8
StarkLeeSunny <307908677@qq.com>
2019-04-28 19:56:27 +0800

    convex
    梳理了思路，完成了凸集的暴力算法，需要完成找切线，如何判断直线是否切割了凸集？目前想的是找凸集的最高和最低点。看直线是否在之间。

--- CONVEX.py
+++ CONVEX.py
@@ -12,6 +12,17 @@ def sign_ret(num):
     else:
         return -1
 
+def split(li):
+    li.sort(key=takefirst)
+    li_temp = [[]]
+    num0 = math.floor(len(li) / 5)
+    for i in range(num0):
+        li_temp[i] = li[i:i + 5]
+
+    if len(li) - 5 * num0 != 0:
+        li_temp.append(li[5 * num0:len(lists)])
+    return li_temp
+
 def takefirst(elem):
     return elem[0]
 
@@ -26,13 +37,14 @@ def find_convex(s0):
 
 def brutehull(li):
     hull=[]
-    pos=0
-    neg=0
+    hull0=[]
+
     for i in range(len(li)):
         for j in range(i+1,len(li)):
             p1=li[i]
             p2=li[j]
-
+            pos = 0
+            neg = 0
             for k in range(len(li)):
                 p3=li[k]
                 if (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1])>=0:
@@ -40,21 +52,23 @@ def brutehull(li):
                 if (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1])<=0:
                     neg = neg+1
             if pos == len(li) or neg == len(li):
-                hull.append(p1)
+                hull0.append(p1)
+                hull0.append(p2)
 
+    for i in hull0:
+        if i not in hull:
+            hull.append(i)
+
+    li.sort(key=takefirst)
     return hull
 
-lists = [[2, 2], [3, 3], [5, 2], [4, 0], [3, 1], [-1, 0], [0, 1], [1, 0], [0, -2]]
-lists.sort(key=takefirst)
-lists_temp=[[]]
-num0= math.floor(len(lists)/5)
-for i in range (num0):
-    lists_temp[i]=lists[i:i+5]
 
-if len(lists) - 5*num0 != 0:
-    lists_temp.append(lists[5*num0:len(lists)])
 
-hull1=brutehull(lists)
+lists = [(2, 2), (3, 3), (5, 2), (4, 0), (3, 1),(-1, 0), (0, 1), (1, 0), (0, -2)];
+
+split_li=split(lists)
+
+hull1=brutehull(split_li[0])
 
 
 print('finished')
