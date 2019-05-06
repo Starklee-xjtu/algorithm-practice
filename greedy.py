a888aeb60b1ffbfed18e11c4586014285d43499f
StarkLeeSunny <307908677@qq.com>
2019-05-07 00:03:17 +0800

    greedy complete
    完成贪心算法

--- greedy.py
+++ greedy.py
@@ -1,30 +1,21 @@
 import numpy as np
 import math
-
-
-
+import copy
 def takefirst(elem):
     return elem[0]
 def takesecond(elem):
     return elem[1]
-
-
-
 d = [(70,4), (60,2), (50,4), (40,3), (30,1), (20,4), (10,6)] #第一项为奖励，第二项为截止时间
-fin =[] #最终选择
-#d.sort(key=takefirst)
+
+fin=[]
+d.sort(key=takefirst,reverse=True)
 for i in range(len(d)):
-
-    if d[i][1]>len(fin):
-        fin.append(d[i])
-
+    fin.append(d[i])
+    temp=copy.deepcopy(fin)
+    temp.sort(key=takesecond)
+    if temp[len(temp)-1][1]<len(temp):
+        print(fin.pop()) #pop 一定要带括号
 fin.sort(key=takesecond)
-
-for i in range(len(fin)):
-    if fin[i][0]>len(fin):
-        fin.append(d[i])
-
-
 print (fin)
 
 
